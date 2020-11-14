62bd2b5262204be177fa0931fda05f36c7a01628
Jose Luis Olivar <jose.olivar@javerianacali.edu.co>
2020-11-13 20:01:30 -0500

    Add CRUD info static local

--- models/bicicleta.js
+++ models/bicicleta.js
@@ -14,8 +14,25 @@ Bicicleta.add = function (aBici) {
     Bicicleta.allBicis.push(aBici);
 }
 
-var a = new Bicicleta(1,'Rojo','Montaña',[3.257129, -76.539517]);
-var b = new Bicicleta(2,'Azul','Urbana',[3.257292, -76.542040]);
+Bicicleta.findById = function (aBiciID) {
+    var aBici = Bicicleta.allBicis.find(x => x.id == aBiciID);
+    if (aBici) {
+        return aBici
+    } else {
+        throw new Error(`No existe una bici con el Id <b>${aBici}</b>`);
+    }
+}
+Bicicleta.removeById = function (aBiciID) {
+    for (var i = 0; i < Bicicleta.allBicis.length; i++) {
+        if (Bicicleta.allBicis[i].id == aBiciID) {
+            Bicicleta.allBicis.splice(i, 1);
+            break;
+        }
+    }
+}
+
+var a = new Bicicleta(1, 'Rojo', 'Montaña', [3.257129, -76.539517]);
+var b = new Bicicleta(2, 'Azul', 'Urbana', [3.257292, -76.542040]);
 Bicicleta.add(a);
 Bicicleta.add(b);
 
--- routes/bicicletas.js
+++ routes/bicicletas.js
@@ -2,7 +2,16 @@ var express = require('express');
 var router = express.Router();
 var bicicletaController = require('../controllers/bicicleta');
 
+//Show
 router.get('/', bicicletaController.bicicleta_list);
+//Create
+router.get('/create', bicicletaController.bicicleta_create_get);
+router.post('/create', bicicletaController.bicicleta_create_post);
+//Update
+router.get('/:id/update', bicicletaController.bicicleta_update_get);
+router.post('/:id/update', bicicletaController.bicicleta_update_post);
+//Delete
+router.post('/:id/delete', bicicletaController.bicicleta_delete_post);
 
 
-module.exports= router; 
+module.exports = router; 
